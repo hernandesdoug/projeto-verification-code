caa94de412e74a272565270dcf6a934d28779633
Douglas Hernandes <douglashernandes@icloud.com>
2025-06-03 11:05:21 -0300

    criacao arquivo js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 function mostraEmail(){
     const email = document.getElementById("frm-email");
-    email.classList.toggle("active");
-    console.log("clicou aqui");
+    const telefone = document.getElementById("frm-phone");
+    email.classList.add("active");
+    telefone.classList.remove("active");    
+    
+}
+
+function mostraTelefone(){
+    const telefone = document.getElementById("frm-phone");
+    const email = document.getElementById("frm-email");
+    telefone.classList.add("active");
+    email.classList.remove("active");
    
 }
 
