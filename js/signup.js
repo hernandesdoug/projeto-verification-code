16065735d830a76d5b561fa71a3d2979e11663e9
Douglas Hernandes <douglashernandes@icloud.com>
2025-08-17 16:54:36 -0300

    renomeando tags e ids

--- js/signup.js
+++ js/signup.js
@@ -2,6 +2,45 @@ function mostraLogin(){
   window.location = "index.html"        
 }
 
+async function signUp(event){
+  event.preventDefault();
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const dateBirth = document.getElementById("date-birth").value;
+  const password = document.getElementById("password").value;
+  const email = document.getElementById("get-email").value;
+  const phoneNumber = document.getElementById("phone-number").value;
+
+  try{
+    const response = await fetch("http://localhost:3333/users",{
+        method:  "POST",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        body: JSON.stringify({
+            firstName,
+            lastName,
+            email,
+            dateBirth,
+            phoneNumber,
+            password})
+    });
+    if(response.ok){
+        const data = await response.json();
+        console.log(data);
+        localStorage.clear();
+        const id = data.id;
+        const url = `verification.html?id=${id}`;
+        window.location.href = url;          
+    }else {
+        const errorData = await response.json();
+        console.error("Login failed", errorData.message);
+        alert(errorData.message);
+    }
+} catch(error){
+    console.error("Unexpected error!", error);
+}
+}
 document.addEventListener("DOMContentLoaded", function () {
   const login= document.getElementById("slider-login");  
   login.addEventListener("click", mostraLogin);
